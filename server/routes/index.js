cf238d6f1661c23dd4cfefe719b98f2c14e07d1a
Anders Blom <anders@andersblom.dk>
2017-10-17 22:09:44 -0400

    testing output

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/user/:username', function(req, res, next) {
   res.json({
-    message: "ok",
+    user: req.params.username,
   });
 });
 
