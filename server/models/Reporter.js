38dcc6167e3e9671a2e4116a950a60c56acc3b23
Anders Blom <anders@andersblom.dk>
2017-10-20 09:55:18 -0400

    Reporter model

---
+++ server/models/Reporter.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+
+const reporterSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: "Please enter a title",
+        trim: true,
+    },
+    description: String,
+    stepsToReproduce: String,
+    contactEmail: {
+        type: String,
+        required: "Please provide us with an email.",
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate:{
+            validator: (value) => {
+              return validator.isEmail(value);  
+            },
+            message:'{VALUE} is not a valid Email'
+       },
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+reporterSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model("Reporter", reporterSchema);
