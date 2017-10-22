77c7bc109b5678cc98b9d1b04fa15619b561cac2
Anders Blom <anders@andersblom.dk>
2017-10-22 16:04:55 -0400

    getSingleReporter controller

--- server/controllers/reporterController.js
+++ server/controllers/reporterController.js
@@ -44,15 +44,33 @@ exports.viewAllReporters = async (req, res) => {
     }
 }
 
-exports.viewSingleReporter = (req, res) => {
+exports.viewSingleReporter = async (req, res) => {
+    try {
+        const reporter = await BugReporter.findOne({
+            _id: req.body.reporterId
+        });
+        
+        if (!reporter) {
+            res.status(204);
+            res.json({
+                message: "No reporter was found for this ID",
+                result: reporter
+            })
+        }
+        
+        res.status(200);
+        res.json({
+            message: "success",
+            result: reporter,
+        });
 
+    } catch(err) {
+        next(err);
+    }
 }
 
 exports.updateReporter = (req, res) => {
 
 }
 
-// router.post('/bugreporter/create', reporterController.createReporter);
-// router.get('/bugreporter', reporterController.viewAllReporters);
-// router.get('/bugreporter/:id', reporterController.viewSingleReporter);
 // router.put('/bugreporter/:id', reporterController.updateReporter);
